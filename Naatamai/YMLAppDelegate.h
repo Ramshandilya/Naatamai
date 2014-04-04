6c12f4217f2820bfe3dc0a9f2cc1b4b3f5765025
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2014-04-04 22:59:58 +0530

    Initial Commit

---
+++ Naatamai/YMLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YMLAppDelegate.h
+//  Naatamai
+//
+//  Created by Ramsundar Shandilya on 04/04/14.
+//  Copyright (c) 2014 Ramsundar Shandilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Naatamai/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Naatamai
+//
+//  Created by Ramsundar Shandilya on 04/04/14.
+//  Copyright (c) 2014 Ramsundar Shandilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YMLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YMLAppDelegate class]));
+    }
+}
